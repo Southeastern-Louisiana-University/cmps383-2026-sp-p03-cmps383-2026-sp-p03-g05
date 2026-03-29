ee4c50bbd53b4703145cbd7c4192dcd8da1adbf9
angeliccodes <angel.njoku@selu.edu>
2026-03-29 13:48:36 -0500

    simplification on payment config

--- Selu383.SP26.Api/Features/Payments/PaymentConfiguration.cs
+++ Selu383.SP26.Api/Features/Payments/PaymentConfiguration.cs
@@ -1,29 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace Selu383.SP26.Api.Features.Payments;
-
-public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+namespace Selu383.SP26.Api.Features.Payments
 {
-    public void Configure(EntityTypeBuilder<Payment> builder)
+    public class PaymentConfiguration
     {
-        
-        builder.ToTable("Payments");
-
-        // Set the primary key
-        builder.HasKey(x => x.Id);
-
-        
-        builder.Property(x => x.CardholderName)
-               .IsRequired()
-               .HasMaxLength(100);
-
-        builder.Property(x => x.LastFourDigits)
-               .IsRequired()
-               .HasMaxLength(4);
-
-        builder.Property(x => x.ExpirationDate)
-               .IsRequired()
-               .HasMaxLength(5); // Format: MM/YY
     }
 }
